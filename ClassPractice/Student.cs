1e2ae74f9908f3eba137796da66f2c60d749067e
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-01 21:57:39 +0000

    add properties

--- ClassPractice/Student.cs
+++ ClassPractice/Student.cs
@@ -4,7 +4,11 @@ namespace ClassPractice
 	public class Student
 	{
 		public string StudentName { get; set; }
-		
+		public string StudentNumber { get; set; }
+		public int Grade1 { get; set; }
+		public int Grade2 { get; set; }
+		public int Grade3 { get; set; }
+
 	}
 }
 
