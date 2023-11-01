b42eab923d295229999b527c9cd33b3284360db6
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-01 22:15:11 +0000

    add constructor

--- ClassPractice/Student.cs
+++ ClassPractice/Student.cs
@@ -9,6 +9,16 @@ namespace ClassPractice
 		public int Grade2 { get; set; }
 		public int Grade3 { get; set; }
 
+
+		//constructor
+		public Student(string studentName, string studentNumber, int grade1, int grade2, int grade3)
+		{
+			StudentName = studentName;
+			StudentNumber = studentNumber;
+			Grade1 = grade1;
+			Grade2 = grade2;
+			Grade3 = grade3;
+		}
 	}
 }
 
