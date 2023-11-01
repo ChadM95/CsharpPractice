92e98b2d80f98cbd1379dbb7fce22736d3c8e2ba
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-01 22:32:29 +0000

    add new object

--- ClassPractice/Program.cs
+++ ClassPractice/Program.cs
@@ -4,7 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        /* Long way of populating an object
+          
+        Student Student1 = new Student();
+
+        Student1.StudentName = "John";
+        Student1.StudentNumber = "S123456";
+        Student1.Grade1 = 45;
+        Student1.Grade2 = 65;
+        Student1.Grade3 = 73;
+
+        Console.WriteLine(Student1);
+        */
+
+
+        Student s2 = new Student("John","S123",45,55,65);
+
+
+
+        Console.WriteLine($"{s2.StudentName} {s2.StudentNumber} {s2.Grade1} {s2.Grade2} {s2.Grade3}");
+        Console.ReadLine();
     }
 }
 
--- ClassPractice/Student.cs
+++ ClassPractice/Student.cs
@@ -3,7 +3,7 @@ namespace ClassPractice
 {
 	public class Student
 	{
-		public string StudentName { get; set; }
+		public string StudentName { get; set; } 
 		public string StudentNumber { get; set; }
 		public int Grade1 { get; set; }
 		public int Grade2 { get; set; }
