c438d8da821df97bcd41894a21a1123c93d01fb1
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-02 22:37:27 +0000

    use override ToString() method

--- ClassPractice/Program.cs
+++ ClassPractice/Program.cs
@@ -4,29 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        /* Long way of populating an object
-          
-        Student Student1 = new Student();
 
-        Student1.StudentName = "John";
-        Student1.StudentNumber = "S123456";
-        Student1.Grade1 = 45;
-        Student1.Grade2 = 65;
-        Student1.Grade3 = 73;
-        */
-
-        //shorter way using constructor
-
-        Student s2 = new Student("John","S123",45,55,65);
-        Console.WriteLine($"{s2.StudentName} {s2.StudentNumber} {s2.Grade1} {s2.Grade2} {s2.Grade3}");
-        
-
-        //add display method
         Student s3 = new Student("Conal","S14141441",40,50,60);
-        s3.DisplayStudent();
 
+        Console.WriteLine(s3);
 
         Console.ReadLine();
+
     }
 }
 
--- ClassPractice/Student.cs
+++ ClassPractice/Student.cs
@@ -3,6 +3,7 @@ namespace ClassPractice
 {
 	public class Student
 	{
+		//properties
 		public string StudentName { get; set; } 
 		public string StudentNumber { get; set; }
 		public int Grade1 { get; set; }
@@ -20,8 +21,13 @@ namespace ClassPractice
 			Grade3 = grade3;
 		}
 
+        public override string ToString() 
+        {
+			return StudentName + " " + StudentNumber + " " + Grade1 + " " + Grade2 + " " + Grade3;
+        }
 
-		public void DisplayStudent()
+        //method within class - Call with ObjectName.DisplayStudent ie s3.DisplayStudent
+        public void DisplayStudent()
 		{
 			Console.WriteLine($"{StudentName} {StudentNumber} {Grade1} {Grade2} {Grade3}");
 		} 
