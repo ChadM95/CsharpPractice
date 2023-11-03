ac7f5db86c012993b93b8ffdb629d6e70ade49e3
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-03 22:26:36 +0000

    create object FirstName LastName

--- ClassPractice/Program.cs
+++ ClassPractice/Program.cs
@@ -5,12 +5,33 @@ class Program
     static void Main(string[] args)
     {
 
-        Student s3 = new Student("Conal","S14141441",40,50,60);
+        Student s1 = new Student("Chad","Murphy");
 
-        Console.WriteLine(s3);
-
+        Console.WriteLine(s1.FirstName + " " + s1.LastName);
         Console.ReadLine();
 
     }
+}
+
+// ----------------------------------------------------------------------------
+
+
+public class Student
+{
+    //outline properties up here
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+
+
+    //constructor
+
+    public Student(string firstName, string lastName) //this is the variable taken in from main method as an argument
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
 }
 
