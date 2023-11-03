9840f90d789fc2713d41e6a96977171b7c2227cc
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-03 22:29:28 +0000

    add second object, use both objects in an output

--- ClassPractice/Program.cs
+++ ClassPractice/Program.cs
@@ -6,8 +6,9 @@ class Program
     {
 
         Student s1 = new Student("Chad","Murphy");
+        Student s2 = new Student("Jad", "Jurphy");
 
-        Console.WriteLine(s1.FirstName + " " + s1.LastName);
+        Console.WriteLine(s1.FirstName + " " + s1.LastName + ", meet " + s2.FirstName + " " + s2.LastName);
         Console.ReadLine();
 
     }
